b5757d8891b6a235309c29d4b2d6fcf56f5cae0f
caoxiaowei0522 <1746637603@qq.com>
2022-08-29 22:00:06 +0800

    更新login

--- libs/login.py
+++ libs/login.py
@@ -1,8 +1,8 @@
 num_pws={'username':'1234','password':'123456'}
 from common.baseAPI import BaseAPI
 class Login(BaseAPI):
-    def login111(self,indata):
+    def login(self,indata):
         resp=self.request_send(data=indata)
         print(resp.text)
 if __name__ == '__main__':
-    Login().login111(num_pws)
+    Login().login(num_pws)
